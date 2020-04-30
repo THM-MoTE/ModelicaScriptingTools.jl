cf57be8b98ec2c46b32ab1b7f6fe59a97e990790
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-04-30 14:58:28 +0200

    adds simulate call to example

--- src/julia/example.jl
+++ src/julia/example.jl
@@ -6,4 +6,5 @@ if !isdir("out")
 end
 omc = MoST.setupOMCSession("out", "res")
 MoST.loadModel(omc, "Example")
+MoST.simulate(omc, "Example", MoST.getSimulationSettings(omc, "Example"))
 MoST.closeOMCSession(omc)
