705606e16544c689ed1f79aaba118e3a10331627
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-04-30 15:05:58 +0200

    extends example

--- src/julia/example.jl
+++ src/julia/example.jl
@@ -1,10 +1,20 @@
 include("./MoST.jl")
 using .MoST
+using Test
 
 if !isdir("out")
     mkdir("out")
 end
+
+if !isdir("regRefData")
+    mkdir("regRefData")
+end
+
 omc = MoST.setupOMCSession("out", "res")
 MoST.loadModel(omc, "Example")
 MoST.simulate(omc, "Example", MoST.getSimulationSettings(omc, "Example"))
+cp("out/Example_res.csv", "regRefData/Example_res.csv"; force=true)
+@testset "Example" begin
+    MoST.testmodel(omc, "Example")
+end
 MoST.closeOMCSession(omc)
