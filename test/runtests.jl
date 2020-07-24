59bd57e94316021dcdceadf6ee10792dc1af4c36
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-07-24 22:04:23 +0200

    moves res folder to test folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ if !isdir("regRefData")
     mkdir("regRefData")
 end
 
-withOMC("out", "../res") do omc
+withOMC("out", "res") do omc
     loadModel(omc, "Example")
     simulate(omc, "Example", getSimulationSettings(omc, "Example"))
     cp("out/Example_res.csv", "regRefData/Example_res.csv"; force=true)
