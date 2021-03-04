c0df1dc5ce22c905271a5b72498414fd3a5eb76b
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-03-04 11:38:14 +0100

    uses --project instead of local import

--- test/failingRegTests.jl
+++ test/failingRegTests.jl
@@ -1,6 +1,4 @@
 # tests that tests in regressionTest fail if regression reference data does not fit simulation data
-# TODO find out if we can somehow run this with Pkg so we don't have to do the local import
-push!(LOAD_PATH,joinpath(dirname(@__FILE__),"../src/"))
 using ModelicaScriptingTools
 using Test
 using CSV
